91c858e428bc59e9968f500b1968e4874a5cdcc3
sindavathar <sebkuras@gmail.com>
2016-01-06 13:08:00 -0500

    Working Slack Demo, Stage 3 not Stage 4 Good base copy.

--- meteorXboxLfg/client/startup/subscribe.js
+++
@@ -1,9 +0,0 @@
-Meteor.subscribe('channels');
-Meteor.subscribe('allUsernames');
-
-Template.messages.onCreated(function() {
-  var self = this;
-  self.autorun(function() {
-    self.subscribe('messages', Session.get('channel'));
-  });
-});
--- meteorXboxLfg/lib/collections/channels.js
+++
@@ -1,1 +0,0 @@
-Channels = new Mongo.Collection("channels");
